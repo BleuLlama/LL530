bd4654f0f1a3c9aaab6f3dfc0eacad1427a50056
Scott Lawrence <scottl@makeway.is>
2019-12-31 15:56:13 -0500

    Added MIT License info.

--- Firmware/LL530_Keyboard_AB_v1/LL530_Keyboard_AB_v1.ino
+++ Firmware/LL530_Keyboard_AB_v1/LL530_Keyboard_AB_v1.ino
@@ -8,6 +8,32 @@
  *  v0.86 2019-02-17 Help-A/B for explore mode
  *  v0.85 2018-12-20 Keyboard working, help key remapping done
  */
+/* 	MIT License
+
+	Copyright (c) 2020 Scott Lawrence
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or
+    sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+
+	The above copyright notice and this permission notice shall
+	be included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+	KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+	WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+	PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+	OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+	OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 
 
 #define kLL530_Version "LL530_Keyboard v0.90"
